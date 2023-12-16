a739ba9dc28856fe3392dcf6ce5fcc051a52ba6c
Misal Azeem <misal.azeem@gmail.com>
2023-12-16 23:03:04 +0500

    fixing linters

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -63,7 +63,7 @@ Devise.setup do |config|
       ['DELETE', %r{^/logout$}]
     ]
     jwt.expiration_time = 30.minutes.to_i
-end
+  end
   # Configure which authentication keys should be case-insensitive.
   # These keys will be downcased upon creating or modifying a user and when used
   # to authenticate or find a user. Default is :email.
